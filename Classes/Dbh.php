6b86dc5d37b28ecea37eaf7ad0a075997f502bde
l0sm1sh9 <m_k0vacev1c@hotmail.com>
2024-10-25 18:01:00 +0200

    project-setup

---
+++ Classes/Dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+class Dbh() {
+    private $host = "localhost";
+    private $dbname = "myfirstdatabase";
+    private $dbusername = "root";
+    private $dbpassword = "";
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # php-oop-project
-mini php oop exercise project
+mini php oop signup system exercise project
