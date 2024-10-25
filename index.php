bf982afa73d06f228f4e258237d57b240afb6987
l0sm1sh9 <m_k0vacev1c@hotmail.com>
2024-10-25 18:18:59 +0200

    includes-setup

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
 
 </head>
 <body>
+
+<form action="includes/signup.inc.php" method="post" >
+    <input type="text" name="username">
+    <input type="password" name="pwd">
+    <button>Signup</button>
+</form>
+
 <?php
 
 ?>
