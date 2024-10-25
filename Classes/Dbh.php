db10fbb26c17530bdf14391a75b284925449f578
l0sm1sh9 <m_k0vacev1c@hotmail.com>
2024-10-25 18:11:23 +0200

    dbh-setup

--- Classes/Dbh.php
+++ Classes/Dbh.php
@@ -5,4 +5,14 @@ class Dbh() {
     private $dbname = "myfirstdatabase";
     private $dbusername = "root";
     private $dbpassword = "";
+
+    protected function connect() {
+    try {
+        $pdo = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->dbusername, $this->dbpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+    }
 }
